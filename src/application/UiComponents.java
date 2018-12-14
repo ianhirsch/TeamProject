ad33b76cca436ca5c911345ee688533f30b23160
IanHirsch <ihirsch@wisc.edu>
2018-12-13 20:58:56 -0600

    filter gui but food count not working?

--- src/application/UiComponents.java
+++ src/application/UiComponents.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -18,6 +19,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.RadioButton;
@@ -25,10 +27,13 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Path;
@@ -44,6 +49,10 @@ public class UiComponents {
 	public ObservableList<FoodItem> foodList = FXCollections.observableArrayList();
 	public ObservableList<FoodItem> mealList = FXCollections.observableArrayList();
 	public ObservableList<Integer> foodCount = FXCollections.observableArrayList();
+	public ObservableList<String> ruleNutrient = FXCollections.observableArrayList();
+	public TextField tf;
+	public String selectedRuleNutrient;
+	public String selectedRuleComparator;
 	public Label labelL = new Label();
 
 	public UiComponents(Stage stage) {
@@ -58,11 +67,11 @@ public class UiComponents {
 		ColumnConstraints column2 = new ColumnConstraints();
 		column2.setPercentWidth(40);
 		ColumnConstraints column3 = new ColumnConstraints();
-        column3.setPercentWidth(40);
-		
-		
+		column3.setPercentWidth(40);
+
+
 		Button addFoodBtn = new Button("Add Food");	
-		
+
 		addFoodBtn.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
 		grid.getColumnConstraints().addAll(column1, column2, column3);
@@ -70,72 +79,145 @@ public class UiComponents {
 		grid.setVgap(10);
 		grid.setPadding(new Insets(10, 10, 10, 10));
 
+		VBox vb = new VBox();
+		vb.getChildren().addAll(creatFilterLabel(), nutrientFilter(), valueFilter(), comparatorFilter(),
+				 addRuleButton(), filterFoodButton(stage));
+		vb.setSpacing(10);
+		vb.setStyle("-fx-background-color: Gainsboro;-fx-border-color: black;");
 
-		grid.add(planMealLabel(), 1, 0);
-		grid.add(filterFoodButton(stage), 2, 4);
-		grid.add(addFoodBtn, 1, 4);
+		grid.add(planMealLabel(), 0, 0);
+		grid.add(addFoodBtn, 1, 1);
 		grid.add(clickLabel(), 2, 1);
 		grid.add(foodItemList(), 1, 2);
-		grid.add(foodCountLabel(), 1, 1);
+		grid.add(foodCountLabel(), 1, 0);
 		grid.add(mealList(), 2, 2);
 		grid.add(loadFoodButton(stage), 1, 3);
 		grid.add(analyzeMealButton(stage), 2, 3);
-		grid.add(radioButton(stage), 0, 2);
+		grid.add(vb, 0, 2);
+		grid.add(ruleList(), 0, 3);
 
 	}
-		public VBox radioButton(Stage primaryStage ) {
-		    ToggleGroup group = new ToggleGroup();
-	        RadioButton calories = new RadioButton("Calories");
-	        RadioButton fat = new RadioButton("Fat");
-	        RadioButton carbohydrate = new RadioButton("Carbohydrate");
-	        RadioButton fiber = new RadioButton("fiber");
-	        RadioButton protein = new RadioButton("protein");
-	        
-	        calories.setToggleGroup(group);
-	        fat.setToggleGroup(group);
-	        carbohydrate.setToggleGroup(group);
-	        fiber.setToggleGroup(group);
-	        protein.setToggleGroup(group);
-		    
-		    VBox vb = new VBox();
-		    vb.setPadding(new Insets(40, 0, 0, 0));
-		    vb.setSpacing(20);
 
-		    vb.getChildren().addAll(calories, fat, carbohydrate, fiber, protein);
-		    
-		    return vb;
-		}
-		
-		
-		
-		
-	
+	public Label creatFilterLabel() {
+		Label lb = new Label("Create Custom Filter");
+		lb.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		lb.setAlignment(Pos.CENTER);
+		lb.setStyle("-fx-border-color: black;");
+		return lb;
+	}
 
-    
+	public VBox nutrientFilter() {
+		ToggleGroup group = new ToggleGroup();
+		RadioButton calories = new RadioButton("Calories");
+		calories.setUserData("Calories");
+		RadioButton fat = new RadioButton("Fat");
+		fat.setUserData("Fat");
+		RadioButton carbohydrate = new RadioButton("Carbohydrate");
+		carbohydrate.setUserData("Carbohydrates");
+		RadioButton fiber = new RadioButton("Fiber");
+		fiber.setUserData("Fiber");
+		RadioButton protein = new RadioButton("Protein");
+		protein.setUserData("Protein");
+
+		calories.setToggleGroup(group);
+		fat.setToggleGroup(group);
+		carbohydrate.setToggleGroup(group);
+		fiber.setToggleGroup(group);
+		protein.setToggleGroup(group);
+
+		VBox vb = new VBox();
+		vb.setPadding(new Insets(10, 0, 0, 10));
+		vb.setSpacing(10);
+
+		vb.getChildren().addAll(calories, fat, carbohydrate, fiber, protein);
+
+		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle,Toggle new_toggle) {
+				selectedRuleNutrient = group.getSelectedToggle().getUserData().toString();
+			}
+		});
+
+		return vb;
+	}
+
+	public TextField valueFilter() {
+		tf = new TextField("Enter a number");
+		return tf;
+	}
+
+	public VBox comparatorFilter() {
+		ToggleGroup group = new ToggleGroup();
+		RadioButton lessEqual = new RadioButton("<=");
+		lessEqual.setUserData("<=");
+		RadioButton equal = new RadioButton("=");
+		equal.setUserData("=");
+		RadioButton greatEqual = new RadioButton(">=");
+		greatEqual.setUserData(">=");
+
+		lessEqual.setToggleGroup(group);
+		equal.setToggleGroup(group);
+		greatEqual.setToggleGroup(group);
+
+		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle,Toggle new_toggle) {
+				selectedRuleComparator = group.getSelectedToggle().getUserData().toString();
+			}
+		});
+
+		VBox vb = new VBox();
+		vb.setPadding(new Insets(10, 0, 0, 10));
+		vb.setSpacing(10);
+		vb.getChildren().addAll(lessEqual, equal, greatEqual);
+
+		return vb;
+	}
+
+	public ListView<String> ruleList() {
+		ListView<String> list = new ListView<String>();
+		list.setItems(ruleNutrient);
+		TableColumn<String,String> column = new TableColumn<String,String>("Rule");
+		//list.getColumns().add(column);
+		column.setCellValueFactory(new PropertyValueFactory<>("name"));
+		return list;
+	}
+
+	public Button addRuleButton() {
+		Button btn = new Button("Add Rule");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(final ActionEvent e) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(selectedRuleNutrient + " ");
+				sb.append(selectedRuleComparator + " ");
+				sb.append(tf.getText());
+				ruleNutrient.add(sb.toString());
+			}});
+		return btn;
+	}
 
 	public Button filterFoodButton(Stage primaryStage) {
-	    Button filterFoodBtn = new Button("Filter Food");
-	    filterFoodBtn.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-	    filterFoodBtn.setOnAction(
-            new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                   
-                    final Stage dialog = new Stage();
-                    dialog.initModality(Modality.APPLICATION_MODAL);
-                    dialog.initOwner(primaryStage);
-                    VBox dialogVbox = new VBox(20);
-                    Button enter = new Button("enter value");
-                    dialogVbox.getChildren().addAll(new Text("Number of Foods in Meal: " ), enter);
-                    Scene dialogScene = new Scene(dialogVbox, 300, 200);
-                    dialog.setScene(dialogScene);
-                    dialog.show();
-                }
-             });
-	    return filterFoodBtn;
-	
+		Button filterFoodBtn = new Button("Filter Food");
+		filterFoodBtn.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		filterFoodBtn.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+
+						final Stage dialog = new Stage();
+						dialog.initModality(Modality.APPLICATION_MODAL);
+						dialog.initOwner(primaryStage);
+						VBox dialogVbox = new VBox(20);
+						Button enter = new Button("enter value");
+						dialogVbox.getChildren().addAll(new Text("Number of Foods in Meal: " ), enter);
+						Scene dialogScene = new Scene(dialogVbox, 300, 200);
+						dialog.setScene(dialogScene);
+						dialog.show();
+					}
+				});
+		return filterFoodBtn;
+
 	}
-	
+
 	public GridPane getGrid() {
 		return grid;
 	}
@@ -151,35 +233,35 @@ public class UiComponents {
 		Button analyzeMealButton = new Button("Analyze Meal");
 		analyzeMealButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		analyzeMealButton.setOnAction(
-		        new EventHandler<ActionEvent>() {
-		            @Override
-		            public void handle(ActionEvent event) {
-		            	double totalCalories = 0;
-		            	double totalFat = 0;
-		            	double totalFiber = 0;
-		            	double totalProtein = 0;
-		            	double totalCarbohydrates = 0;
-		
-		            	for(int i = 0; i < mealList.size(); i++) {
-		            		totalCalories = totalCalories + mealList.get(i).getNutrientValue("calories");
-		            		totalFat = totalFat + mealList.get(i).getNutrientValue("fat");
-		            		totalFiber = totalFiber + mealList.get(i).getNutrientValue("fiber");
-		            		totalCarbohydrates = totalCarbohydrates + mealList.get(i).getNutrientValue("carbohydrate");
-		            		totalProtein = totalProtein + mealList.get(i).getNutrientValue("protein");
-		            	}
-		                final Stage dialog = new Stage();
-		                dialog.initModality(Modality.APPLICATION_MODAL);
-		                dialog.initOwner(primaryStage);
-		                VBox dialogVbox = new VBox(20);
-		                dialogVbox.getChildren().add(new Text("Number of Foods in Meal: " + mealList.size() + 
-		                		"\n" + "Total Calories: " + totalCalories + "\n" + "Total Fat: " + totalFat +  "\n" 
-		                		+ "Total Carbohydrates: " + totalCarbohydrates + "\n" + "Total Fiber: " + totalFiber
-		                		+ "\n" + "Total Protein: " + totalProtein + "\n\n" + "END OF ANALYSIS"));
-		                Scene dialogScene = new Scene(dialogVbox, 300, 200);
-		                dialog.setScene(dialogScene);
-		                dialog.show();
-		            }
-		         });
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						double totalCalories = 0;
+						double totalFat = 0;
+						double totalFiber = 0;
+						double totalProtein = 0;
+						double totalCarbohydrates = 0;
+
+						for(int i = 0; i < mealList.size(); i++) {
+							totalCalories = totalCalories + mealList.get(i).getNutrientValue("calories");
+							totalFat = totalFat + mealList.get(i).getNutrientValue("fat");
+							totalFiber = totalFiber + mealList.get(i).getNutrientValue("fiber");
+							totalCarbohydrates = totalCarbohydrates + mealList.get(i).getNutrientValue("carbohydrate");
+							totalProtein = totalProtein + mealList.get(i).getNutrientValue("protein");
+						}
+						final Stage dialog = new Stage();
+						dialog.initModality(Modality.APPLICATION_MODAL);
+						dialog.initOwner(primaryStage);
+						VBox dialogVbox = new VBox(20);
+						dialogVbox.getChildren().add(new Text("Number of Foods in Meal: " + mealList.size() + 
+								"\n" + "Total Calories: " + totalCalories + "\n" + "Total Fat: " + totalFat +  "\n" 
+								+ "Total Carbohydrates: " + totalCarbohydrates + "\n" + "Total Fiber: " + totalFiber
+								+ "\n" + "Total Protein: " + totalProtein + "\n\n" + "END OF ANALYSIS"));
+						Scene dialogScene = new Scene(dialogVbox, 300, 200);
+						dialog.setScene(dialogScene);
+						dialog.show();
+					}
+				});
 		return analyzeMealButton;
 	}
 
@@ -233,16 +315,16 @@ public class UiComponents {
 		return btn;
 	}
 
-//	public Button addFoodMenu() {
-//		Button addFood = new Button("Add Food");
-//		addFood.setOnAction(new EventHandler<ActionEvent>() {
-//			@Override
-//			public void handle(final ActionEvent e) {
-//				
-//			}
-//				
-//		});
-//	}
+	//	public Button addFoodMenu() {
+	//		Button addFood = new Button("Add Food");
+	//		addFood.setOnAction(new EventHandler<ActionEvent>() {
+	//			@Override
+	//			public void handle(final ActionEvent e) {
+	//				
+	//			}
+	//				
+	//		});
+	//	}
 
 	private TableView<FoodItem> foodItemList() {
 		TableView<FoodItem> tableL = new TableView<FoodItem>();
@@ -252,7 +334,7 @@ public class UiComponents {
 
 		FoodData initialFoodData = new FoodData();
 		initialFoodData.loadFoodItems(Paths.get(System.getProperty("user.dir"), "foodItems.csv").toString());
-		
+
 		for (FoodItem foodItem : initialFoodData.getAllFoodItems()) {
 			foodList.add(foodItem);
 		}
@@ -262,14 +344,14 @@ public class UiComponents {
 		foodCount.add(initialFoodData.getAllFoodItems().size());
 		tableL.getColumns().add(foodColumn);
 		tableL.setRowFactory(tv -> {
-		    TableRow<FoodItem> row = new TableRow<>();
-		    row.setOnMouseClicked(event -> {
-		        if (event.getClickCount() == 2 && (!row.isEmpty())) {
-		            FoodItem selectedFood = row.getItem();
-		            mealList.add(selectedFood);
-		        }
-		    });
-		    return row;
+			TableRow<FoodItem> row = new TableRow<>();
+			row.setOnMouseClicked(event -> {
+				if (event.getClickCount() == 2 && (!row.isEmpty())) {
+					FoodItem selectedFood = row.getItem();
+					mealList.add(selectedFood);
+				}
+			});
+			return row;
 		});
 		return tableL;
 	}
@@ -278,7 +360,7 @@ public class UiComponents {
 		TableView<FoodItem> table = new TableView<FoodItem>();
 		TableColumn<FoodItem, String> mealColumn = new TableColumn<FoodItem, String>("Meal");
 		mealColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-	    table.setItems(mealList);
+		table.setItems(mealList);
 		table.getColumns().add(mealColumn);
 		return table;
 	}
